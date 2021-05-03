2bfc4c0d9e3379bec16781ac0a1657acb947dd07
hasaku63 <takuyahashimoto1988@gmail.com>
2021-05-03 21:51:11 +0900

    another version

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,25 @@ fn main() {
     println!("Please inptu your guess."); 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    // io::stdin().read_line(&mut guess)
+    //     .expect("Failed to read line");
 
     // こっちでも動く
     // let stdin = io::stdin();
     // stdin.read_line(&mut guess)
     //     .expect("failed to read line.");
 
-    println!("You guessed: {}", guess);
+    // Result を明示的にばらして扱う。これでも動くが、 match arm の Ok, Err の引数のバインドはいらないので
+    // unused variable の警告が出てしまう。
+    let ret = io::stdin().read_line(&mut guess);
+    match ret {
+        Ok(sz) => {
+            println!("You guessed: {}", guess);
+        }
+        Err(e) => {
+            panic!("Failed to read line");
+        }
+    }
+
+    // println!("You guessed: {}", guess);
 }
